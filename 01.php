ee6b78f86fba4a2eb410f920b983e5abbba98499
Juan <juanfelipecuatro9@gmail.com>
2021-07-27 19:43:33 -0400

    firsts steps in POO, define a class and add its variables, with its contents

--- 01.php
+++ 01.php
@@ -1,4 +1,29 @@
 <?php include 'includes/header.php';
 
+// Definir una clase
+class Producto {
+    public $nombre;
+    public $precio;
+    public $disponible;
+}
+
+$producto = new Producto();
+$producto->nombre = 'Tablet';
+$producto->precio = 200;
+$producto->disponible = true;
+
+
+echo ("<pre>");
+var_dump($producto);
+echo ("</pre>");
+
+$producto2 = new Producto();
+$producto2->nombre = 'Mouse';
+$producto2->precio = 100;
+$producto2->disponible = false;
+
+echo ("<pre>");
+var_dump($producto2);
+echo ("</pre>");
 
 include 'includes/footer.php';
