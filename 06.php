581006f2493caa10d7c39de6931935dfd0b87991
Juan <juanfelipecuatro9@gmail.com>
2021-08-07 12:41:05 -0400

    i set a new thing, a interface, it help me to know what methods have a class

--- 06.php
+++ 06.php
@@ -1,4 +1,28 @@
 <?php include 'includes/header.php';
 
+//Interface
+
+interface TransporteInterfaz {
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+
+
+abstract class Transporte implements TransporteInterfaz{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+
+    public function getInfo() : string{
+        return "El transporte tiene " . $this->ruedas . " y una capacidad de " . $this->capacidad . " personas";
+    }
+
+    public function getRuedas() : int{
+        return $this->ruedas;
+    }
+}
+
+
 
 include 'includes/footer.php';
