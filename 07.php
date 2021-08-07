88a229fe221b1bfac8b53bb1fcdff4eb7eb160d3
Juan <juanfelipecuatro9@gmail.com>
2021-08-07 13:25:06 -0400

    i learned about polimorfisme, is uses same methodos in diferents class

--- 07.php
+++ 07.php
@@ -1,4 +1,61 @@
-<?php include 'includes/header.php';
+<?php
+
+// use Transporte as GlobalTransporte;
+// use TransporteInterfaz as GlobalTransporteInterfaz;
+
+include 'includes/header.php';
+
+//Polimorfismo
+
+interface TransporteInterfaz {
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+
+
+ class Transporte implements TransporteInterfaz{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+
+    public function getInfo() : string{
+        return "El transporte tiene " . $this->ruedas . " y una capacidad de " . $this->capacidad . " personas";
+    }
+
+    public function getRuedas() : int{
+        return $this->ruedas;
+    }
+}
+
+class Automovil extends Transporte implements TransporteInterfaz{
+    public function __construct(protected int $ruedas, protected int $capacidad, protected string $color)
+    {
+        
+    }
+
+    public function getInfo() : string{
+        return "El transporte Auto tiene " . $this->ruedas . " y una capacidad de " . $this->capacidad . " personas y su color " . $this->color;
+    }
+
+    public function getColor() : string{
+        return "El color es " . $this->color;
+    }
+}
+
+echo ("<pre>");
+
+var_dump($transporte = new Transporte(4, 4));
+var_dump($auto = new Automovil(4, 4, "Rojo"));
+
+echo ($transporte->getInfo());
+echo ("<br>");
+echo ($auto->getInfo());
+echo ("<br>");
+echo ($auto->getColor());
+
+
+echo ("</pre>");
 
 
 include 'includes/footer.php';
