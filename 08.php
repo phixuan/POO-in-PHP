9aa6f443fbab850a74d680d44d59b1f1ab504672
Juan <juanfelipecuatro9@gmail.com>
2021-08-07 13:50:39 -0400

    i include classes in 08, in 2 form; automatically and manually

--- 08.php
+++ 08.php
@@ -1,4 +1,26 @@
 <?php include 'includes/header.php';
 
+//Incluir las clases
+
+// require 'clases/clientes.php';
+// require 'clases/detalles.php';
+
+
+//Otra forma />
+
+function mi_autoload($clase){
+    echo ($clase);
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+//  Otra forma /<
+
+
+$cliente = new Clientes();
+echo ("<br>");
+$detalles = new Detalles();
+
 
 include 'includes/footer.php';
