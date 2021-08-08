abd1d38fc695ab935c6b22deaecd0de63194493e
Juan <juanfelipecuatro9@gmail.com>
2021-08-08 16:20:19 -0400

    i consult to DB with POO, using classes how mysqli

--- 09.php
+++ 09.php
@@ -1,4 +1,30 @@
 <?php include 'includes/header.php';
 
+//Connect to BD with mysqli.
+
+$db = new mysqli('localhost', 'root', 'root', 'bienes_raices');
+
+//Create query
+$query = "SELECT titulo, imagen FROM propiedades";
+$stmt = $db->prepare($query);
+
+
+//execute
+$stmt->execute();
+
+//create variable
+$stmt->bind_result($titulo, $img);
+
+//assign result
+$stmt->fetch();
+
+
+//Imprimimos el resultado
+while($stmt->fetch()):
+    var_dump($titulo);
+    var_dump($img);
+    echo ("<br>");
+endwhile;
+
 
 include 'includes/footer.php';
